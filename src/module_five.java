d45a7fb38ba966cc26df2d4b5866fb0832c6d23e
catuchi <atuchibueze@gmail.com>
2024-07-10 13:29:43 -0400

    interfaces

--- src/module_five.java
+++ src/module_five.java
@@ -5,10 +5,67 @@
 //			methods declaration;
 //		}
 
+class PC {
+	public void myPrintMethod() {
+		System.out.println("Parent method");
+	}
+}
+
+class CC extends PC {
+	@Override
+	public void myPrintMethod() {
+//		super.myPrintMethod(); 					// If i want to call the method from the parent
+		System.out.println("Child Method");
+	}
+}
+
 public class module_five {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
-
+		CC child = new CC();
+		child.myPrintMethod();
+		
+		PC parent = new PC();
+		parent.myPrintMethod();
+		
+		A5 obj = new A5();
+		obj.print();
+		
+		A6 obj1 = new A6();
+		obj1.display();
 	}
 }
+
+// Interfaces
+// Interfaces have static variables and abstract methods (methods without bodies)
+
+interface printable{
+	void print();
+}
+
+class A5 implements printable{
+	public void print() {
+		System.out.println("Hello");
+	}
+}
+
+// extending interfaces
+interface wallet{
+	int money = 400;
+	String item1 = "Picture";
+	String item2 = "Credit Card";
+}
+
+interface walletDetails{
+	void display();
+}
+
+class A6 implements walletDetails{
+	public void display() {
+		System.out.printf("I have a %s, my %s and $%d dollars in my pocket", wallet.item1, wallet.item2, wallet.money);
+	}
+}
+
+
+
---
+++ src/module_six.java
@@ -0,0 +1,11 @@
+
+// Polymorphism
+
+public class module_six {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+
+}
