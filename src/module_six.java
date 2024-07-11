06e2b826c28d14f5c2055d792ef04d3c5770aadb
catuchi <atuchibueze@gmail.com>
2024-07-11 12:20:18 -0400

    polymorphism labs

--- src/module_six.java
+++ src/module_six.java
@@ -8,7 +8,13 @@ public class module_six {
 		
 		System.out.println(add(1,3));
 		System.out.println(add("Sub", "scriber"));
-
+		
+//		Derived obj = new Derived();
+		
+		Geeks obj1 = new Geeks();
+		GFG obj2 = new Geeks();
+		obj1.getData();
+		obj2.getData();
 	}
 	
 	// method overloading
@@ -20,3 +26,39 @@ public class module_six {
 		return a + b;
 	}
 }
+
+//
+abstract class Base {
+	Base(){
+		System.out.println("Base Constructor called");
+	}
+	abstract void fun();
+}
+
+class Derived extends Base {
+	Derived(){
+		System.out.println("Derived Constructor called");
+	}
+	void fun() {
+		System.out.println("Derived fun() called");
+	}
+}
+
+//
+class GFG {
+	protected void getData() {
+		System.out.println("Inside GFG");
+	}
+}
+
+class Geeks extends GFG{
+	protected void getData() {
+		System.out.println("Inside Geeks");
+	}
+}
+
+
+
+
+
+
