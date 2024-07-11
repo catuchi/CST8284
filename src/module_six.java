fd2444a1f670235a8a4324ffa7a4febe00b26973
catuchi <atuchibueze@gmail.com>
2024-07-11 11:21:14 -0400

    polymorphism

--- src/module_six.java
+++ src/module_six.java
@@ -5,7 +5,18 @@ public class module_six {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
+		
+		System.out.println(add(1,3));
+		System.out.println(add("Sub", "scriber"));
 
 	}
-
+	
+	// method overloading
+	public static int add(int a, int b) {
+		return a + b;
+	}
+	
+	public static String add(String a, String b) {
+		return a + b;
+	}
 }
